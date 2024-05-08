4d19143e4b074accc891b4d2c07c918199cb9530
pj925 <leepeijunn@gmail.com>
2024-05-08 15:14:10 +0800

    verison 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.pitchify_main"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pitchify_main"
