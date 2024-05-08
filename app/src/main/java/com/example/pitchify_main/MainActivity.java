d7ccddf9a8320b793a315dd7d82a866a53537230
pj925 <leepeijunn@gmail.com>
2024-05-08 15:28:51 +0800

    verison 2

--- app/src/main/java/com/example/pitchify_main/MainActivity.java
+++ app/src/main/java/com/example/pitchify_main/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login6);
+        setContentView(R.layout.aipitching_feedback;
     }
 }
