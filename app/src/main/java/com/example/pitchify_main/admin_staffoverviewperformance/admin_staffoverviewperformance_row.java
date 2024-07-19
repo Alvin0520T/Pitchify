9756711a29c985773093a87ea814b477748a2e2b
Alvin Tang <alvintang0520@gmail.com>
2024-07-20 02:58:56 +0800

    alvin de

--- app/src/main/java/com/example/pitchify_main/admin_staffoverviewperformance/admin_staffoverviewperformance_row.java
+++ app/src/main/java/com/example/pitchify_main/admin_staffoverviewperformance/admin_staffoverviewperformance_row.java
@@ -1,12 +1,17 @@
 package com.example.pitchify_main.admin_staffoverviewperformance;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.pitchify_main.R;
+import com.example.pitchify_main.dashboard_screens.onboarding3;
+import com.example.pitchify_main.dashboard_screens.onboarding4;
 import com.example.pitchify_main.data.CustomAdapter;
 import com.example.pitchify_main.data.PitchifyDBHelper;
 import com.example.pitchify_main.model.User;
@@ -22,6 +27,16 @@ public class admin_staffoverviewperformance_row extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_staffoverviewperformance_row);
+        ImageButton nextButton = findViewById(R.id.imageButton);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(admin_staffoverviewperformance_row.this, admin_profile2.class);
+                startActivity(intent);
+
+            }
+        });
 
         recyclerView = findViewById(R.id.recyclerView);
         pitchifyDBHelper = new PitchifyDBHelper(admin_staffoverviewperformance_row.this);
